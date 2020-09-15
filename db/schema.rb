be6f9d215b72f1212003ab67bcfe9fcf92c8cba7
Jivefox <https://github.com/Jivefox>
2020-09-14 21:18:14 -0600

    Added Category model and join table for products_categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_135617) do
+ActiveRecord::Schema.define(version: 2020_09_15_031211) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -21,4 +27,13 @@ ActiveRecord::Schema.define(version: 2020_09_14_135617) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "products_categories", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_categories_on_category_id"
+    t.index ["product_id"], name: "index_products_categories_on_product_id"
+  end
+
+  add_foreign_key "products_categories", "categories"
+  add_foreign_key "products_categories", "products"
 end
