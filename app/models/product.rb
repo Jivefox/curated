18ad62807e3610010db02783a07de442f4a4935e
Jivefox <https://github.com/Jivefox>
2020-09-21 09:55:14 -0600

    fixed product#categories_attributes= so not to add blank categories to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,10 +9,10 @@ class Product < ApplicationRecord
     # accepts_nested_attributes_for :categories
 
     def categories_attributes=(category_attributes)
-        binding.pry
         category_attributes.values.each do |category_attribute|
-          category = Category.find_or_create_by(category_attribute)
-          self.categories << category unless category.name.blank?
+            category = Category.find_or_create_by(category_attribute) unless category_attribute[:name].blank?
+            self.categories << category unless category.nil?
+            end 
         end
     end
 
