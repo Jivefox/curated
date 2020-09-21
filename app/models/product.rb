53f02daa1ef44ac813ae773de08300b9eea12d2c
Jivefox <https://github.com/Jivefox>
2020-09-21 10:05:13 -0600

    added custom text for submit button dependant upon route/controller action

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
     has_many :categories, through: :products_categories, dependent: :destroy
 
     # validates_with CategoryValidator
-    validates :categories, presence: true, unless: :category_attributes_present?
+    validates :categories, presence: true
     validates :name, presence: true
     validates :price, numericality: true
     # accepts_nested_attributes_for :categories
@@ -12,11 +12,6 @@ class Product < ApplicationRecord
         category_attributes.values.each do |category_attribute|
             category = Category.find_or_create_by(category_attribute) unless category_attribute[:name].blank?
             self.categories << category unless category.nil?
-            end 
         end
     end
-
-    # def category_attributes_present?
-    #     category_attributes.present?
-    # end
 end
