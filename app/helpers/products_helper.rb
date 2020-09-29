e4dd0910eb4b2cefcd64019e06b4deb7c120ab02
Jivefox <https://github.com/Jivefox>
2020-09-29 10:27:43 -0600

    made changes to products index page to show different indices for admin/common user *needs fixing*

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,13 +1,2 @@
 module ProductsHelper
-    def sold_products
-        @products.map do |product|
-            product if !product.available 
-        end
-    end
-
-    def pending_approval
-        @products.map do |product|
-            product if !product.approved
-        end
-    end
 end
