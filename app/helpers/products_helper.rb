4421b755ebda24b01278f6182297df79f04505d1
Jivefox <https://github.com/Jivefox>
2020-09-28 22:21:02 -0600

    added helper methods for products

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,13 @@
 module ProductsHelper
+    def sold_products
+        @products.map do |product|
+            product if !product.available 
+        end
+    end
+
+    def pending_approval
+        @products.map do |product|
+            product if !product.approved
+        end
+    end
 end
