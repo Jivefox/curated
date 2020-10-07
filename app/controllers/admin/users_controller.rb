a635dd6359202a30390892b4b5c0de2f9ed99271
Jivefox <https://github.com/Jivefox>
2020-10-07 09:02:57 -0600

    added authorization before_action to admin::user_controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,19 @@
 class Admin::UsersController < ApplicationController
     before_action :authenticate_user!
-
+    before_action :authorized?
+    
     def show
         @user = User.find(params[:id])
         @pending_products = Product.pending_products
+    end
+
+    private
+
+    def authorized?
+      unless current_admin?
+        flash[:error] = "What are you doing here?  You can't be here."
+        redirect_to root_path
+      end
     end
 
 end
