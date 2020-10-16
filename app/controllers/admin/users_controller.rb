44592486e7456681627e923e02a08a7725c52c36
Jivefox <https://github.com/Jivefox>
2020-10-16 09:35:50 -0600

    removed duplicate flash messages and changed flash alert for admin page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,7 +11,7 @@ class Admin::UsersController < ApplicationController
 
     def authorized?
       unless current_admin?
-        flash[:error] = "What are you doing here?  You can't be here."
+        flash[:alert] = "What are you doing?  You can't go there."
         redirect_to root_path
       end
     end
